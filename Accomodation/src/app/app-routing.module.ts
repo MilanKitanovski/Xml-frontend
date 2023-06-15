bdefa2db76becd6d9a5800510e843141c652d606
MilanKitanovski <93132392+MilanKitanovski@users.noreply.github.com>
2023-06-15 15:09:15 +0200

    Add guard to profile

--- Accomodation/src/app/app-routing.module.ts
+++ Accomodation/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./modules/pages/home/home.component";
 import {LogInComponent} from "./modules/pages/log-in/log-in.component";
 import {RegisterComponent} from "./modules/pages/register/register.component";
+import {ProfileComponent} from "./modules/components/profile/profile.component";
+import {AuthGuardGuard} from "./shared/auth-guard.guard";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LogInComponent },
   { path: 'register', component: RegisterComponent },
-  //{path: 'user-profile', component: UserProfileComponent, canActivate:[AuthGuardGuard], data: {role: ['HOST', 'GUEST']}},
+  {path: 'profile', component: ProfileComponent, canActivate:[AuthGuardGuard], data: {role: ['HOST', 'GUEST']}},
 ];
 
 @NgModule({
