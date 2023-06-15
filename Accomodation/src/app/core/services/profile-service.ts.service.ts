30dfe6f5bda6d6e8c386133c118f67cb4eaa57b0
MilanKitanovski <93132392+MilanKitanovski@users.noreply.github.com>
2023-06-15 14:42:27 +0200

    Update User info

--- Accomodation/src/app/core/services/profile-service.ts.service.ts
+++ Accomodation/src/app/core/services/profile-service.ts.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import {SignInRequestPayload} from "../../modules/pages/log-in/login-request";
+import {Observable} from "rxjs";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {UserProfileDto} from "../dtos/userDto";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileServiceTsService {
 
-  constructor() { }
+  baseUrl = 'http://localhost:5245'
+
+  constructor(private http: HttpClient) { }
+
+
+  changeUser(user: UserProfileDto): Observable<any>{
+    let headers = new HttpHeaders({
+      "Content-Type" : "application/json",
+      "Authorization" : "Bearer " + localStorage.getItem("token"), //autorizacija
+    });
+    let options = {headers:headers};
+    return this.http.put(this.baseUrl +'/api/users/update-user/' + user.id, user, options);
+  }
+
 }
